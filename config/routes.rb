aab1227385da90c31e51dab506e3f2e955c2ad36
Max Jilu Wang <wangjilu23@gmail.com>
2022-03-10 15:02:16 +1100

    added select method when load homepage

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cards, only: [:show, :index] do
     collection do
-      get :compare, :analysis
+      get :compare, :analysis, :select
     end
   end
   resources :users
