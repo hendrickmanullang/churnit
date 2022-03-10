9c7721ec836a686a1fb11ee94193c31f274fb8f5
Hendrick Manullang <h.manullang@gmail.com>
2022-03-11 08:19:52 +1100

    finalised scoring method

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -14,20 +14,40 @@ class CardsController < ApplicationController
     session[:onboarding][:spending] = params[:spending] if params[:spending]
     session[:onboarding][:card_selected] = params[:card_selected] if params[:card_selected]
 
+    # Truncate the list of cards by excluding cards where minimum income exceeds user's income
+    @truncated_list = Card.where('minimum_income <= ?', params[:income])
+
+    # Apply a DYNAMIC SORT (user determined):
+
+    ## Call order of drag & drop result from home page using $( "#sortable" ).sortable('toArray');
+    ## This should result in an array of string consisting: ['fee', 'cashback', 'reward', 'foreign']
+    ## Take the index of each string, and use active record queries to sort recommended cards
+    ## The active record syntax will be be:
+
+    ### fee = .order(annual_fee_after: :asc)
+    ### cashback = .order(cashback_reward: :desc)
+    ### reward = .order(bonus_point_reward: :desc)  -> need to find a way to standardise this value across all reward programs
+    ### foreign = .order(foreign_transaction_fee: :asc)
+
+    ### apply the order of preference below (will need to rewrite this code):
+
+    @sortedcards1 = @truncated_list.order(annual_fee_init: :asc)
+    @sortedcards2 = @sortedcards1.order(cashback_reward: :desc)
+    @sortedcards3 = @sortedcards2.order(bonus_point_reward: :desc)
+    @sortedcards4 = @sortedcards3.order(foreign_transaction_fee: :asc)
+
+    ### Determine ChurnIT score (this will need to be reworked to take into account user's spending)
+    @churnit_score = 'interest_free_period - annual_fee_after - interest_rate * 100 - late_payment_fee - foreign_transaction_fee * 100'
+
+    # Apply ChurnIT score and STATIC SORT:
+    @recommended_cards = @sortedcards4.order(Arel.sql(@churnit_score))
+
+    # once completed, refactor using the Card Selector service
+    # below is a sample syntax
     # @recommended_cards = CardSelector.new(
     #                         session[:onboarding][:income],
     #                         session[:onboarding][:spending]
     #                       ).call
-
-    # # order_1 = :user_input_in_home
-    # # order_2 = :user_input_in_home
-    # # order_3 = :user_input_in_home
-    # # order_4 = :user_input_in_home
-
-    @recommended_cards = Card.where('minimum_income <= ?', params[:income])
-
-    # # session[:recommended_cards] = @cards.map{|card| card.id}
-    # # save the ids of the cards to the session[:cards] in an array of id's
   end
 
   def select
