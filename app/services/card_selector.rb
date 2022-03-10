9534af1b714b1fdf7a30695a519203be431a4f02
Max Jilu Wang <wangjilu23@gmail.com>
2022-03-10 14:58:56 +1100

    need to work on call and sort method for sorting algorithm

--- app/services/card_selector.rb
+++ app/services/card_selector.rb
@@ -1,11 +1,17 @@
 class CardSelector
-  def initialize(income, spending)
+  def initialize(income, spending, card_selected)
     @income = income
     @spending = spending
+    @card_selected = card_selected
   end
 
   def call
     # run algorithm and return card ids
+    # sort taking variables of spending and income
     return Card.all
+  end
+
+  def sort
+    puts "sorting"
   end
 end
