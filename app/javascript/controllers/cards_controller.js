dff9052dec778bedc8533f12e07e69f175762839
Max Jilu Wang <wangjilu23@gmail.com>
2022-03-16 13:43:14 +1100

    fixed sorting and filtering funciton, now they shuffling shuffling

--- app/javascript/controllers/cards_controller.js
+++ app/javascript/controllers/cards_controller.js
@@ -28,26 +28,11 @@ const $grid = $('.grid').isotope({
     name: '.name',
     reward_rate: '.reward_rate',
     interest_free_period: '.interest_free_period toExponential(2)',
-    annual_fee_init: '.annual_fee_init'
+    annual_fee_init: '.annual_fee_init',
   }
 });
 
-// bind filter button click
 $('#filters').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  // use filterFn if matches value
-  console.log('filter clicked');
-  filterValue = filterFns[ filterValue ] || filterValue;
-  $grid.isotope({ filter: filterValue });
+  let filterValue = $( this ).attr('data-filter');
+  $grid.isotope( { filter: filterValue});
 });
-// filter function
-const filterFns = {
-  visaCards: function() {
-    let visaCards = $(this).find('.card_type').text();
-      return visaCards.match( /VISA/ )
-  },
-  cardType: function() {
-    let cardType = $(this).find('.card_type').text();
-      return cardType.match( /Mastercard/ )
-  }
-}
